b83d98e6e3e3949907f1ef4eab6184f5f380cf4b
Dip_San <khadkasandip489@gmail.com>
2025-11-04 09:39:01 +0545

    Task 4

---
+++ Task 4/Program.cs
@@ -0,0 +1,38 @@
+﻿// Task 4
+class Program
+{
+    static void Main()
+    {
+        int[] array = { 3,4,5,6,7 };
+
+        // Array.Sort()
+        Array.Sort(array);
+        Console.WriteLine("Sorted array:");
+        foreach (int num in array)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine("\n");
+
+        // Array.Reverse()
+        Array.Reverse(array);
+        Console.WriteLine("Reversed array:");
+        foreach (int num in array)
+        {
+            Console.Write(num + " ");
+        }
+        Console.WriteLine("\n");
+
+        // Loop to print elements
+        Console.WriteLine("Elements using for loop:");
+        for (int i = 0; i < array.Length; i++)
+        {
+            Console.WriteLine(array[i]);
+        }
+
+        // Array.IndexOf() to find the position of specific element
+        int index = Array.IndexOf(array, 4);
+        Console.WriteLine("\nIndex of 4 is: " + index);
+    }
+}
+
