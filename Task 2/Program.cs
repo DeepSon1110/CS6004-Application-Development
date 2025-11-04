5860ffeac3e271cfad13b468645cfb7881d9642c
Dip_San <khadkasandip489@gmail.com>
2025-11-04 09:23:37 +0545

    Task 2

---
+++ Task 2/Program.cs
@@ -0,0 +1,31 @@
+﻿class Circle
+{
+    public const double pi = 3.14;
+
+    //method to calculate the area and perimeter of circle
+    public static double Area(double radius)
+    {
+        return pi * radius * radius;
+    }
+
+    public static double Perimeter(double radius)
+    {
+        return 2 * pi * radius;
+    }
+
+}
+
+class Program
+{
+    public static void Main(String[] args)
+    {
+        double radius = 4;
+        double Area = Circle.Area(radius);
+        double Perimeter = Circle.Perimeter(radius);
+
+        Console.WriteLine($"Area of Circle is {Area}.");
+        Console.WriteLine($"Perimeter of Circle is {Perimeter}.");
+
+    }
+}
+
