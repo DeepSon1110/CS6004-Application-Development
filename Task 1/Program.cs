082722ba9ed9cba4b94c4aa1e6c444c43c9dc3ba
Dip_San <khadkasandip489@gmail.com>
2025-11-04 09:07:51 +0545

    Task 1

---
+++ Task 1/Program.cs
@@ -0,0 +1,12 @@
+﻿// Task 1 :
+// To declare a variable userName and initialize it
+String userName;
+userName = "Sandip Khadka";
+
+
+// To declare a variable luckyNumber and initialize it 
+int luckyNumber;
+luckyNumber = 2;
+
+//
+Console.WriteLine($"Hello, {userName}! Your luckyNumber is {luckyNumber}");
