25c14d764c1bcbccfeb5c0d9aeeae44110bde12f
Dip_San <khadkasandip489@gmail.com>
2025-11-04 09:41:51 +0545

    Task 5

---
+++ Task 5/Program.cs
@@ -0,0 +1,27 @@
+﻿//Task 5
+class Program
+{
+    static void Main()
+    {
+        //DateTime variable for birthdate
+        DateTime birthDate = new DateTime(2005, 06, 03); 
+
+        //DateTime variable for the current date and time
+        DateTime currentDate = DateTime.Now;
+
+        //Calculate age using TimeSpan
+        TimeSpan ageSpan = currentDate - birthDate;
+
+        //Convert age from days to years (approximate)
+        int ageInYears = (int)(ageSpan.TotalDays / 365.25);
+
+        //Print birthdate, current date, and age
+        Console.WriteLine("Birthdate: " + birthDate.ToShortDateString());
+        Console.WriteLine("Current Date: " + currentDate.ToShortDateString());
+        Console.WriteLine("Age: " + ageInYears + " years");
+
+        //Add 10 days to birthdate 
+        DateTime newDate = birthDate.AddDays(10);
+        Console.WriteLine("Birthdate + 10 days: " + newDate.ToShortDateString());
+    }
+}
